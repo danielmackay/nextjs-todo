baa03a382d4c8a3294b47321c0e7dba2b22bd3fe
Daniel Mackay <daniel.mackay@levo.com.au>
2022-08-26 21:50:35 +1000

    Added enter key support for adding a new todo

--- components/todos/todoAdd.tsx
+++ components/todos/todoAdd.tsx
@@ -5,33 +5,40 @@ import { useState } from 'react';
 import { TodoItem } from '../../models/todoItem';
 
 interface TodoProps {
-  onAddTodo: (added:TodoItem) => void
+  onAddTodo: (added: TodoItem) => void
 }
 
 const TodoAdd = (props: TodoProps) => {
   const [todoText, setTodoText] = useState("");
 
-  const onAdd = () =>{
-    let todo:TodoItem = {
+  const onAdd = () => {
+    let todo: TodoItem = {
       id: 999,
-      title: todoText, 
+      title: todoText,
       isComplete: false,
     };
 
     props.onAddTodo(todo);
+    setTodoText("");
+  }
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.code === "Enter" || e.code === "NumpadEnter") {
+      onAdd();
+    }
   }
 
   return (
-    <form>
-        <Grid container spacing={2} sx={{mb: 4}}>
-          <Grid item sm={10} xs={12}>
-            <TextField label="Add your todo" variant="standard" fullWidth={true} value={todoText} onChange={(e) => setTodoText(e.target.value)}></TextField>
-          </Grid>
-          <Grid item sm={2} xs={12}>
-            <Button color="primary" variant="contained" fullWidth={true} sx={{mt: 2}} onClick={() => onAdd()} disabled={!todoText}>Add</Button>
-          </Grid>
+    // <form>
+      <Grid container spacing={2} sx={{ mb: 4 }}>
+        <Grid item sm={10} xs={12}>
+          <TextField label="Add your todo" variant="standard" fullWidth={true} value={todoText} onChange={(e) => setTodoText(e.target.value)} onKeyDown={(e) => onKeyDown(e)}></TextField>
         </Grid>
-    </form>
+        <Grid item sm={2} xs={12}>
+          <Button color="primary" variant="contained" fullWidth={true} sx={{ mt: 2 }} onClick={() => onAdd()} disabled={!todoText}>Add</Button>
+        </Grid>
+      </Grid>
+    // </form>
   );
 }
 
