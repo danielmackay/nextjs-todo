b09e8d7d2efbfe56662823353e5689c089837e0f
Daniel Mackay <daniel.mackay@levo.com.au>
2022-08-30 16:14:00 +1000

    🐛 Fixed not found error with todo details

--- pages/todo-items/[id].tsx
+++ pages/todo-items/[id].tsx
@@ -7,7 +7,7 @@ import TodoDetails from '../../components/todos/todoDetails'
 const TodoItem: NextPage = () => {
   const router = useRouter()
   const { id } = router.query
-
+  
   const getTodoItemId = () => {
     if (!id || Array.isArray(id))
       return 0;
@@ -15,14 +15,16 @@ const TodoItem: NextPage = () => {
     return parseInt(id);
   }
 
+  const todoItemId = getTodoItemId();
+
   return (
     <>
       <Head>
         <title>Todo Details</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Typography variant='h2' sx={{mb: 2}}>Todo Details</Typography>
-      <TodoDetails todoItemId={getTodoItemId()} />
+      <Typography variant='h2' sx={{ mb: 2 }}>Todo Details</Typography>
+      {!!todoItemId && <TodoDetails todoItemId={todoItemId} />}
     </>
   )
 }
